3b5a1473f381047311ce2cd12f52a1efa5712757
Nauman Saleem <numanbhatti@gmail.com>
2026-02-09 11:34:55 +0300

    - updated the package file

--- Package.swift
+++ Package.swift
@@ -1,27 +1,32 @@
 // swift-tools-version:5.3
 
+// swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         .library(
             name: "DateToolsSwift",
-            targets: ["DateToolsSwift"])
+            targets: ["DateToolsSwift"]
+        )
     ],
     targets: [
         .target(
             name: "DateToolsSwift",
             path: "DateToolsSwift/DateTools",
             exclude: [
-                "Examples",
-                "DateToolsSwift/Examples"
+                "Examples"
             ],
             resources: [
-                .copy("DateTools.bundle")
+                .process("DateTools.bundle")
             ]
         ),
         .testTarget(
@@ -30,9 +35,6 @@ let package = Package(
             path: "DateToolsSwift/Tests/DateToolsTests/DateToolsTestsTests",
             exclude: [
                 "Info.plist"
-            ],
-            resources: [
-                .copy("DateTools.bundle")
             ]
         )
     ]
